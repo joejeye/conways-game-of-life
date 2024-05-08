d5d81faac64d1a0052c1efb67b1ae16310272cc4
J. Jeye <joseph.jeye@gmail.com>
2024-05-08 03:59:02 -0400

    Improve the error message

--- cli/parse.go
+++ cli/parse.go
@@ -15,8 +15,8 @@ func ParseArgs(args []string) (numRows, numCols, init int, err error) {
 	numericArgs := []int{}
 	for i, arg := range args {
 		num, err := strconv.Atoi(arg)
-		if err != nil || (i <= 1 && num < 1) {
-			return 0, 0, 0, fmt.Errorf("invalid input argument (%s) at the %d-th position (0-indexed)", arg, i)
+		if err != nil || (i <= 1 && num < 1) || (i == 2 && num < 0) {
+			return 0, 0, 0, fmt.Errorf("invalid input argument (%s) at the %s position", arg, Num2Ord(i+1))
 		}
 		numericArgs = append(numericArgs, num)
 	}
@@ -40,4 +40,25 @@ func ParseNumericArgs(args []int) (numRows, numCols, init int) {
 	} else {
 		return args[0], args[1], args[2]
 	}
+}
+
+func Num2Ord(num int) string {
+	if num < 0 {
+		panic("Num2Ord: input number must be non-negative")
+	}
+
+	var suffix string
+
+	switch num % 10 {
+	case 1:
+		suffix = "st"
+	case 2:
+		suffix = "nd"
+	case 3:
+		suffix = "rd"
+	default:
+		suffix = "th"
+	}
+
+	return fmt.Sprintf("%d%s", num, suffix)
 }
