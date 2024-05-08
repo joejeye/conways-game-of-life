4b5558228abff8cfaf08c405d2371276d785e6b1
J. Jeye <joseph.jeye@gmail.com>
2024-05-08 16:08:09 -0400

    Update the visualization

--- cmd/main.go
+++ cmd/main.go
@@ -19,17 +19,36 @@ func main() {
 	b.RandomInit(init)
 	c := b.Clone()
 
+	fmt.Println("\033[2J") // Clear the terminal
+
+	bLoop := cells.NewBoard(numRows, numCols)
+
+	// Use fast and slow pointers to detect a loop
 	for {
+		// The fast pointer moves two steps at a time
 		c.Next()
 		c.Next()
+
 		vis := b.TerminalVisualize()
-		fmt.Print("\x0c", vis)
+		moveUp := fmt.Sprintf("\x1b[%dA", numRows+1)
+		fmt.Print(moveUp, vis) // Move the cursor up to the top of the terminal and print the visualization
+
+		// Mark the first convergence point
 		if b.Equal(c) {
+			bLoop = b.Clone()
+		}
+
+		// The slow pointer moves one step at a time
+		b.Next()
+
+		// The marked convergence point is reached again, implying
+		// that the 2D world has gone through at least one cycle
+		if b.Equal(bLoop) {
 			break
 		}
-		b.Next()
-		time.Sleep(500 * time.Millisecond)
+
+		time.Sleep(125 * time.Millisecond)
 	}
 
-	fmt.Println("Loop detected!")
+	fmt.Println("The world entered a loop!")
 }
